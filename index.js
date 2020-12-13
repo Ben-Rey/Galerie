33129fba8da8c7d76211f65c899214eaeea1b59c
Ben <benjamin.reynes.dev@gmail.com>
2020-12-13 20:06:22 +0100

    grid reesponsive

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.onload = start;
 function start() {
   const mainContainer = document.getElementById("main-container");
   addPicturesToDiv(pictures, mainContainer);
+  // Webcam.snap();
 }
 
 function addPicturesToDiv(pictures, div) {
@@ -15,6 +16,19 @@ let pictures = [
   {
     url:
       "https://cdn.pixabay.com/photo/2020/12/03/12/35/sunset-5800386_1280.jpg",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2020/12/04/16/08/moss-5803607_1280.jpg",
+  },
+
+  {
+    url:
+      "https://cdn.pixabay.com/photo/2020/11/22/08/06/forest-5765878_1280.jpg",
+  },
+
+  {
+    url:
+      "https://cdn.pixabay.com/photo/2020/08/30/09/22/people-5528959_1280.jpg",
   },
   {
     url: "https://cdn.pixabay.com/photo/2020/12/04/16/08/moss-5803607_1280.jpg",
